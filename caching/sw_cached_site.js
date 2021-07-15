6076fa3588c23a8622b9d3e87a9bd3fa8c363d29
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2021-07-15 16:09:20 +0700

    Add "SW Caching" demo

---
+++ caching/sw_cached_site.js
@@ -0,0 +1,43 @@
+const cacheName = 'v2';
+
+// Call Install Event
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
+});
+
+// Call Activate Event
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activated');
+  // Remove unwanted caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', (event) => {
+  console.log('Service Worker: Fetching');
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Make copy/clone of response
+        const resClone = response.clone();
+        // Open cache
+        caches.open(cacheName).then((cache) => {
+          // Add response to cache
+          cache.put(event.request, resClone);
+        });
+        return response;
+      })
+      .catch((error) => caches.match(event.request).then((response) => response))
+  );
+});
